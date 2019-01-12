963696a83fef985fa1d1930d4311ce4b6da36665
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-01-12 20:02:55 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <div>Mount Components Here
+  <ControlledInput /> 
+  </div>,
   document.getElementById('root')
 );

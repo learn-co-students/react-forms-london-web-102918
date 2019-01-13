979e2dd11cb867c35cd5eb24bb55c422451f1a1c
paolov1928 <ventura.paolo@gmail.com>
2019-01-13 16:27:01 +0000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,19 @@
 // Code ControlledInput Component Here
+import React, {Component} from 'react';
+
+
+export default class Form extends Component {
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" name="firstName" value={this.state.firstName} />
+        <input type="text" name="lastName" value={this.state.lastName} />
+      </form>
+    )
+  }
+}
